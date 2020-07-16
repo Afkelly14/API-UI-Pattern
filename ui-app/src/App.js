b17273ee3f19e7475201b4713f1651feef8cc06e
Alex_Kelly <afkelly14@Gmail.com>
2020-07-15 20:54:54 -0400

    lab changes. One character name shows up in the console only.

--- ui-app/src/App.js
+++ ui-app/src/App.js
@@ -29,8 +29,13 @@ class App extends Component {
     return (
       <React.Fragment>
         <h1>Breaking Bad</h1>
-        <p>{this.state.characters[0]}</p>
-        <button onClick={this.nextCharacter}>Character</button>
+        <p>{this.state.characters[this.state.currentCount]}</p>
+        <button className="leftButton" onClick={this.lastCharacter}>
+          Go back
+        </button>
+        <button className="rightButton" onClick={this.nextCharacter}>
+          Go forward
+        </button>
       </React.Fragment>
     );
   }
@@ -43,19 +48,31 @@ class App extends Component {
         this.setState({
           name: convertedResponse,
         });
-        console.log(this.state.name[this.state.currentCount]); //gets character in the array
+        console.log(this.state.name[this.state.currentCount].name); //gets character in the array
       });
   }
 
-  nextCharacter = () =>
-    fetch(url, options)
-      .then((res) => res.json())
-      .then((convertedResponse) => {
-        console.log(convertedResponse);
-        this.setState({
-          characters: convertedResponse.characters,
-        });
+  //function to slide to the next character
+  nextCharacter = () => {
+    // if (this.state.currentCount < this.state.characters.length - 1) {
+    this.setState({
+      currentCount: this.state.currentCount + 1,
+    });
+  };
+  //   } else {
+  //     this.setState({
+  //       currentCount: 0,
+  //     });
+  //   }
+  // };
+
+  lastCharacter = () => {
+    if (this.state.currentCount > this.state.characters.length + 1) {
+      this.setState({
+        currentCount: this.state.currentCount - 1,
       });
+    }
+  };
 }
 
 export default App;
