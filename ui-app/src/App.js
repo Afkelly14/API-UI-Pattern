ca1770f1ab1deafd4db005bf3875b7a9f98341cd
Alex_Kelly <afkelly14@Gmail.com>
2020-07-15 20:22:07 -0400

    character info shows up in the console log

--- ui-app/src/App.js
+++ ui-app/src/App.js
@@ -1,9 +1,61 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+//link the api
+
+let url = "https://breakingbadapi.com/api/characters";
+
+const options = {
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+  },
+};
+
+// function App() {
+//   return <div className="App"></div>;
+// }
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: [],
+      currentCount: 0,
+      characters: [],
+    };
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Breaking Bad</h1>
+        <p>{this.state.characters[0]}</p>
+        <button onClick={this.nextCharacter}>Character</button>
+      </React.Fragment>
+    );
+  }
+
+  componentDidMount() {
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((convertedResponse) => {
+        console.log(convertedResponse);
+        this.setState({
+          name: convertedResponse,
+        });
+        console.log(this.state.name[this.state.currentCount]); //gets character in the array
+      });
+  }
+
+  nextCharacter = () =>
+    fetch(url, options)
+      .then((res) => res.json())
+      .then((convertedResponse) => {
+        console.log(convertedResponse);
+        this.setState({
+          characters: convertedResponse.characters,
+        });
+      });
 }
 
 export default App;
---
+++ ui-app/src/name.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Name extends React.Component {
+  render() {
+    let name = this.props.name;
+
+    return <div className="text">{name}</div>;
+  }
+}
+
+export default Name;
